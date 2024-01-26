99c8836edcbf5641d327d1126715e014e6250088
Barba2k2 <lorenzo.dz@hotmail.com>
2024-01-26 20:55:17 -0300

    fixed message bot size

--- lib/src/utils/chat_manager.dart
+++ lib/src/utils/chat_manager.dart
@@ -1,25 +1,62 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:uuid/uuid.dart';
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class ChatManager {
-  bool isLoading = false;
   List<types.Message> messages = [];
   final user = const types.User(id: 'user');
-  final bot = const types.User(id: 'model', firstName: 'Gamini');
+  final bot = const types.User(id: 'model', firstName: 'Gemini');
+  bool isLoading = false;
+  WebSocketChannel? channel;
+  bool isConversationActive = true;
+
+  void initializeWebsocket() {
+    try {
+      channel = IOWebSocketChannel.connect('ws://10.0.2.2:8000/ws');
+    } catch (e) {
+      debugPrint('WebSocket connection failed: $e');
+    }
+  }
+
+  void disposeWebsocket() {
+    channel?.sink.close();
+  }
 
   void addMessage(types.Message message) {
     messages.insert(0, message);
     isLoading = true;
     if (message is types.TextMessage) {
+      var messageJson = json.encode({'text': message.text});
+      debugPrint(messageJson.toString());
+      channel?.sink.add(messageJson);
+    }
+  }
+
+  // Adicione uma nova variável de membro na classe ChatManager
+  String _bufferedMessage = '';
+
+  void onMessageReceived(String response) {
+    if (response.endsWith('.')) {
+      // Se a resposta termina com um ponto, assuma que é o final da mensagem
+      _bufferedMessage += response; // Concatena a parte recebida
       messages.insert(
         0,
         types.TextMessage(
           author: bot,
           createdAt: DateTime.now().millisecondsSinceEpoch,
           id: const Uuid().v4(),
-          text: '',
+          text: _bufferedMessage, // Use a mensagem completa
         ),
       );
+      _bufferedMessage = ''; // Limpa o buffer para a próxima mensagem
+    } else {
+      // Se não for o fim da mensagem, apenas adicione ao buffer
+      _bufferedMessage += response;
     }
+    isLoading = false;
   }
 }
--- lib/src/views/chat_page.dart
+++ lib/src/views/chat_page.dart
@@ -1,20 +1,44 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
-import 'package:gamini_ai_chat/src/utils/chat_manager.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:uuid/uuid.dart';
+import '../utils/chat_manager.dart';
 
 class ChatPage extends StatefulWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  const ChatPage({super.key});
 
   @override
   State<ChatPage> createState() => _ChatPageState();
 }
 
 class _ChatPageState extends State<ChatPage> {
-  
   final ChatManager chatManager = ChatManager();
+
+  @override
+  void initState() {
+    super.initState();
+    chatManager.initializeWebsocket();
+    chatManager.channel?.stream.listen((event) {
+      try {
+        final Map<String, dynamic> data = json.decode(event);
+        final String text = data['text'];
+        chatManager.onMessageReceived(text);
+      } on FormatException {
+        chatManager.onMessageReceived(event);
+      }
+      // Atualize o estado da UI
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    chatManager.disposeWebsocket();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,16 +54,16 @@ class _ChatPageState extends State<ChatPage> {
           backgroundColor: Colors.black,
           inputBorderRadius: BorderRadius.zero,
           receivedMessageBodyTextStyle: TextStyle(color: Colors.white),
-          secondaryColor: Color(0xFF1C1C1C),
+          secondaryColor: Color(0xFF1c1c1c),
           attachmentButtonIcon: Icon(
             Icons.camera_alt_outlined,
             color: Colors.white,
           ),
-          inputBackgroundColor: Color(0xFF1C1C1C),
+          inputBackgroundColor: Color(0xFF1c1c1c),
           seenIcon: Text(
             'read',
             style: TextStyle(
-              fontSize: 12,
+              fontSize: 10.0,
             ),
           ),
         ),
@@ -51,17 +75,18 @@ class _ChatPageState extends State<ChatPage> {
     if (!chatManager.isLoading) {
       final textMessage = types.TextMessage(
         author: chatManager.user,
-        createdAt: DateTime.now().microsecondsSinceEpoch,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
         id: const Uuid().v4(),
-        text: message.text
+        text: message.text,
       );
       chatManager.addMessage(textMessage);
+      setState(() {});
     }
   }
 
   void _handleImageSelection() async {
     final result = await ImagePicker().pickImage(
-      imageQuality: 100,
+      imageQuality: 70,
       maxWidth: 1440,
       source: ImageSource.gallery,
     );
@@ -72,7 +97,7 @@ class _ChatPageState extends State<ChatPage> {
 
       final message = types.ImageMessage(
         author: chatManager.user,
-        createdAt: DateTime.now().microsecondsSinceEpoch,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
         height: image.height.toDouble(),
         id: const Uuid().v4(),
         name: result.name,
@@ -80,6 +105,8 @@ class _ChatPageState extends State<ChatPage> {
         uri: result.path,
         width: image.width.toDouble(),
       );
+
+      chatManager.addMessage(message);
     }
   }
 }
